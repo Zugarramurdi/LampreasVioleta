990d6a6fb8b5631fec5c0b2d2015508cb6febca8
Zugarramurdi <juanma.segura.canales@gmail.com>
2025-12-04 17:00:42 +0100

    Comienzo ejercicio entregable con clases Comercial y Repartidor

---
+++ src/main/java/model/Comercial.java
@@ -0,0 +1,33 @@
+package model;
+
+// --------- CLASE DE EJERCICIO ENTREGABLE -----------
+/*
+ *   2- Implementar DAO para esta clase
+ *           - Interface con metodos CRUD
+ *           - Integrar clase en ComercialView
+ *   3- Actualizar programa para incluir opciones que gestionen esta clase (Alta, consulta, listado y eliminacion)
+ *   4- NUEVA FUNCION:
+ *       - Usando libreria Jackson (ObjectMapper).
+ *       - Exportar a un JSON bien estructurado la lista completa de una de las entidades -> ObjectMapper.writeValueAsString() o writeValue()
+ *       - El fichero debe generarse en la carpeta del proyecto.
+ *
+ *     ---  ENTREGABLES  ---
+ *   1- Codigo fuente completo del proyecto
+ *   2- Ficheros de persistencia (TXT/CSV/JSON) usados
+ *   3- Documento breve (1 pagina) explicando:
+ *       - Las clases nuevas añadidas
+ *       - Su relacion con las existentes
+ *       - Como funciona la exportacion a JSON
+ *
+ *       ---- CRITERIOS DE EVALUACION ---
+ *   * Creacion correcta de las nuevas clases:       2 Puntos
+ *   * DAO e implementacion de persistencia:         3 Puntos
+ *   * Integracion en el menu del programa:          2 Puntos
+ *   * Exportacion funcional a JSON con Jackson:     2 Puntos
+ *   * Orden, claridad y buenas practicas:           1 Punto
+ *       TOTAL                                       10 Puntos
+ *
+ * */
+
+public class Comercial {
+}

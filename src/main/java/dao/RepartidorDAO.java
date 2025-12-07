72f8186b384daa47dbdba42b214b247fd846cb77
Zugarramurdi <juanma.segura.canales@gmail.com>
2025-12-07 11:36:25 +0100

    RepartidorDAO completo

--- src/main/java/dao/RepartidorDAO.java
+++ src/main/java/dao/RepartidorDAO.java
@@ -1,5 +1,16 @@
 package dao;
 
+import DB.DBConnection;
+import model.Repartidor;
+
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RepartidorDAO {
 
     private static final String INSERT_SQL = "INSERT INTO repartidor (id, nombre, telefono, matricula) VALUES" + "(?, ?, ?, ?)";
@@ -16,6 +27,85 @@ public class RepartidorDAO {
                 OR matricula ILIKE ?
             ORDER BY id
             """;
+
+    public void insert(Repartidor repartidor) throws SQLException {
+        try(Connection con = DBConnection.getConnection(); PreparedStatement ps = con.prepareStatement(INSERT_SQL)){
+            ps.setInt(1, repartidor.getId());
+            ps.setString(2, repartidor.getNombre());
+            ps.setString(3, repartidor.getTelefono());
+            ps.setString(4, repartidor.getMatricula());
+
+            ps.executeUpdate();
+        }
+    }
+
+    public Repartidor findById(int id) throws SQLException {
+        try(Connection con = DBConnection.getConnection(); PreparedStatement ps = con.prepareStatement(SELECT_BY_ID_SQL)){
+            ps.setInt(1, id);
+
+            try(ResultSet rs = ps.executeQuery()){
+                if(rs.next()){
+                    return mapRow(rs);
+                }
+                return null;
+            }
+        }
+    }
+
+    public List<Repartidor> findAll() throws SQLException {
+        List<Repartidor> repartidores = new ArrayList<>();
+        try(Connection con = DBConnection.getConnection(); PreparedStatement ps = con.prepareStatement(SELECT_ALL_SQL); ResultSet rs = ps.executeQuery()){
+            while(rs.next()){
+                repartidores.add(mapRow(rs));
+            }
+        }
+        return repartidores;
+    }
+
+    public int Update(Repartidor repartidor) throws SQLException {
+        try(Connection con = DBConnection.getConnection(); PreparedStatement ps = con.prepareStatement(UPDATE_SQL)){
+            ps.setString(1, repartidor.getNombre());
+            ps.setString(2, repartidor.getTelefono());
+            ps.setString(3, repartidor.getMatricula());
+
+            return ps.executeUpdate();
+        }
+    }
+
+    public int deleteById(int id) throws SQLException {
+        try(Connection con = DBConnection.getConnection(); PreparedStatement ps = con.prepareStatement(DELETE_SQL)){
+            ps.setInt(1, id);
+
+            return ps.executeUpdate();
+        }
+    }
+
+    public List<Repartidor> search(String filtro) throws SQLException {
+        String patron = "%"+filtro+"%";
+        try(Connection con = DBConnection.getConnection(); PreparedStatement ps = con.prepareStatement(SEARCH_SQL)){
+            ps.setString(1, patron);
+            ps.setString(2, patron);
+            ps.setString(3, patron);
+
+            List<Repartidor> repartidores = new ArrayList<>();
+            try(ResultSet rs = ps.executeQuery()){
+                while(rs.next()){
+                    repartidores.add(mapRow(rs));
+                }
+            }
+            return repartidores;
+        }
+    }
+
+    private Repartidor mapRow(ResultSet rs) throws SQLException {
+        Repartidor repartidor = new Repartidor(
+                rs.getInt("id"),
+                rs.getString("nombre"),
+                rs.getString("telefono"),
+                rs.getString("matricula")
+        );
+        return repartidor;
+    }
 
 
 
